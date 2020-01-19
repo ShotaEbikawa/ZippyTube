2dfc342b6bafc9205a1528907115b7ad3f5d95c6
ShotaEbikawa <shotaebikawa@gmail.com>
2020-01-19 00:29:33 -0800

    finished developing the Banner component

--- zippytube/src/components/banner/HomeBanner.js
+++ zippytube/src/components/banner/HomeBanner.js
@@ -2,18 +2,30 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 import { makeStyles, withTheme } from '@material-ui/core/styles'; 
 import Grid from '@material-ui/core/Grid'
+import Image from './siu.jpg'
 import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     bannerImage: {
-        height:'300px',
-        display: 'flex',
+        height:'250px',
     },
     quote: {
-        display:'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontFamily: ''
+        backgroundSize: '100% 100%',
+        background: `url(${Image}) no-repeat`,
+        width:'45%'
+    },
+    uniBanner: {
+        width: '100%',
+        height: '100%',
+        objectFit:'cover'
+    },
+    videoContainer: {
+        overflow: 'hidden',
+        width:'55%',
+        height:'250px',
+    },
+    videoStyle: {
+        objectFit: 'cover',
     }
 }))
 
@@ -24,19 +36,16 @@ const HomeBanner = () => {
         <>
         <br/>
         <Grid className={classes.bannerImage} container>
-            <Grid item>
-            <video className={classes.videoStyle} width={500} controls autoplay>
-                <source
-                    src='http://zippytube.s3.us-west-1.amazonaws.com/Southern Illinois University Commercial - Experience SIU.mp4'
-                    type='video/mp4'
-                >
-                </source>
-            </video>
+            <Grid item className={classes.videoContainer}>
+                <video className={classes.videoStyle} width={'100%'} height={'100%'} controls muted autoPlay>
+                    <source
+                        src='http://zippytube.s3.us-west-1.amazonaws.com/Southern Illinois University Commercial - Experience SIU.mp4'
+                        type='video/mp4'
+                    >
+                    </source>
+                </video>
             </Grid>
             <Grid className={classes.quote} items>
-                    <Typography variant='h4'>
-                        Experience SIU
-                    </Typography>
             </Grid>
         </Grid>
         </>
